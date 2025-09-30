7ec3149bb0b47e6ac05897686a6e844a3b4b6669
shubham-92 <158573766+shubham-92@users.noreply.github.com>
2025-09-30 10:48:33 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score="45"
-console.log(typeof score)
+//console.log(typeof score)
 
 let noScore=Number(score)
 
-console.log(typeof noScore)
-console.log(noScore)
+//console.log(typeof noScore)
+//console.log(noScore)
 
 // "33"=> 33
 //"33abc"=>NaN
@@ -13,12 +13,33 @@ let IsLoggedIn=788
 let booleanIsLoggedIn= Boolean(IsLoggedIn)
 let stringLog=String(IsLoggedIn)
 
-console.log(stringLog)
+//console.log(stringLog)
 
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 /*
 1=>true ; 0=>false
 ""=>false
 "shubham"=>true
+*/
+
+//*********************Operations */
+
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2-2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1="Hello "
+let str2="Shubham"
+
+let str3=str1+str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+
+/*
+in these tricky Q we take first one type in priority
 */
