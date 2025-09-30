c23720878d1b483297348a0763588e41326ec2c5
shubham-92 <158573766+shubham-92@users.noreply.github.com>
2025-09-30 10:30:28 +0000

    Convertion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score="45"
+console.log(typeof score)
+
+let noScore=Number(score)
+
+console.log(typeof noScore)
+console.log(noScore)
+
+// "33"=> 33
+//"33abc"=>NaN
+
+let IsLoggedIn=788
+let booleanIsLoggedIn= Boolean(IsLoggedIn)
+let stringLog=String(IsLoggedIn)
+
+console.log(stringLog)
+
+console.log(booleanIsLoggedIn)
+
+/*
+1=>true ; 0=>false
+""=>false
+"shubham"=>true
+*/
