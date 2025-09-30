1945ab10ab315858948598cb718bb665a4690596
shubham-92 <158573766+shubham-92@users.noreply.github.com>
2025-09-30 06:56:47 +0000

    container added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId=14453
+let accountEmail="abcd@gmail.com"
+let accountPass="12114"
+accountCity="delhi"
+let accountState
+console.table([accountEmail,accountId,accountPass,accountCity,accountState])
