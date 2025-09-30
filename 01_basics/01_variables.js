8633473a5e013fdc925c6f49eae08653a6e13b0a
shubham-92 <158573766+shubham-92@users.noreply.github.com>
2025-09-30 07:06:21 +0000

    Update variables file

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,4 +3,6 @@ let accountEmail="abcd@gmail.com"
 let accountPass="12114"
 accountCity="delhi"
 let accountState
+
+// dont use var bc have prb with scope ans functional scope
 console.table([accountEmail,accountId,accountPass,accountCity,accountState])
