bd2a7961da438d670d4814f358eb5fe18cf89217
shubham-92 <158573766+shubham-92@users.noreply.github.com>
2025-09-30 10:59:39 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,30 @@
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+// console.log(2<=1)
+// console.log(2>=1)
+
+console.log("2">1)
+console.log("02">1)
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+
+//=== strict equal check type and val
+
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
+
