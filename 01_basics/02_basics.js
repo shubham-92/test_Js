0bf4ad3e3d8089d540b571ccc659bdb1661562dd
shubham-92 <158573766+shubham-92@users.noreply.github.com>
2025-09-30 07:30:49 +0000

    variables

---
+++ 01_basics/02_basics.js
@@ -0,0 +1,16 @@
+"use strict" //treat all JS code as newer version
+
+//number
+//bigint
+//string
+//boolean=>true/false
+//null=>standalone value
+//undefined
+//symbol=>unique
+
+//object=>collection of key value pairs
+//array=>ordered collection of items
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+
