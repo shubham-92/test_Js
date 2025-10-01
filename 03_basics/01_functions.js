cc78c19982ebf0590cdfcb19ce50c1f5b362ef74
shubham-92 <158573766+shubham-92@users.noreply.github.com>
2025-10-01 12:16:34 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,43 @@
+function sayMyName(){
+    console.log("S");
+    console.log("H");
+    console.log("U");
+    console.log("B");
+    console.log("H");
+    console.log("A");
+    console.log("M");
+
+}
+
+// sayMyName =>refrence
+//sayMyName() => execution
+
+console.log(sayMyName())
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+
+function loginUserMessage(username = "sam"){ //default but can be overwrite by new call
+    if(!username){
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage("hitesh"))
