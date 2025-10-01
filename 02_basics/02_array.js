301f146bccbff51cf3ead5f9ecff06f7e2917ac4
shubham-92 <158573766+shubham-92@users.noreply.github.com>
2025-10-01 10:44:42 +0000

    objects

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -16,3 +16,13 @@ const all_new_heros = [...marvel_heros, ...dc_heros] // use spread func
 const another_array=[1,2,3,[4,5,6],7,[6,7,[4,5]]]
 const usable_another_Array= another_array.flat(Infinity)
 console.log(usable_another_Array)
+
+console.log(Array.isArray("Hitesh"))
+console.log(Array.from("Hitesh"))
+console.log(Array.from({name: "hitesh"})) // interesting, have to mention which part we want to make array
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
---
+++ 02_basics/03_objects.js
@@ -0,0 +1,40 @@
+//constructor=>singleton  object.create
+// literals=>not singleton
+
+
+//object literals
+
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Shubham",
+    "full name": "Shubham Singh jalwal",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "shubham@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser["name"])
+// console.log(JsUser[mySym])
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+
+JsUser.email="ssj@gmail.com"
+
+JsUser.greeting=function(){
+    console.log("hallow Js user!")
+}
+
+JsUser.greetingTwo=function(){
+    console.log(`hallow Js user ${this["full name"]}`) //this.full name can be used also
+}
+
+console.log(JsUser.greeting())
+console.log(JsUser.greetingTwo())
+
+console.log(JsUser)
