8c5e576d0e472d36467cdee4bd79a8332c5050e9
shubham-92 <158573766+shubham-92@users.noreply.github.com>
2025-10-01 14:02:40 +0000

    arrow

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,46 @@
+const user = {
+    username: "hitesh",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+ //user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai =function(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+// chai()
+
+// const chai = () =>{
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// const addTwo=(num1 ,num2)=>{
+//     return num1+num2
+// }
+
+// const addTwo = (num1, num2) => num1+num2   //implict return 
+
+const addTwo = (num1, num2) => (num1+num2) 
+
+// object ko wrap krna hai ( ) isme 
+
+console.log(addTwo(2,3))
