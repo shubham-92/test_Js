e6d0c27292ba8441138c417c3566b91465dd311c
shubham-92 <158573766+shubham-92@users.noreply.github.com>
2025-10-01 12:31:16 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -12,7 +12,8 @@ function sayMyName(){
 // sayMyName =>refrence
 //sayMyName() => execution
 
-console.log(sayMyName())
+//
+//console.log(sayMyName())
 
 // function addTwoNumbers(number1, number2){
 
@@ -41,3 +42,40 @@ function loginUserMessage(username = "sam"){ //default but can be overwrite by n
 
 // console.log(loginUserMessage("hitesh"))
 // console.log(loginUserMessage("hitesh"))
+
+
+function calculateCartPrice(...num1){ //now ... work as rest funct 
+    return num1
+}
+
+
+function calculateCartPrice(val1,val2,...num1){ //1st elem goes to val1 next to val2, baki in num1
+    return num1
+}
+console.log(calculateCartPrice(200,100,125,750))
+
+
+
+const user = {
+    username: "laptop",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
