4a3bed5ae637adda2c574197b989543d733579ab
shubham-92 <158573766+shubham-92@users.noreply.github.com>
2025-10-01 13:02:08 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,58 @@
+// let var const
+
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username="Shubham"
+
+    function two(){
+        const website="vscode"
+        console.log(`my name is ${username} ans i am using ${website}`)
+    }
+    two()
+}
+
+
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
