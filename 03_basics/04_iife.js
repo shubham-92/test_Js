ec4330ca4881ba707c46d2787182612148d4673c
shubham-92 <158573766+shubham-92@users.noreply.github.com>
2025-10-02 13:18:29 +0000

    control flow

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -8,3 +8,17 @@
 ((name) => {
     console.log(`DB connected ${name}`)
 })("mongoDb")
+
+// An IIFE (Immediately Invoked Function Expression) is a JavaScript function that runs as soon as it's defined.
+//  It's primarily used to create a private scope for variables, preventing them from polluting the global environment.
+
+
+// code execution
+// 1.JS creates Global excution context 
+// 2.Next it creates memory phase
+// 3.Memory phase - In this phase,the variables are set to undefined 
+//   until the execution phase(next phase) and the functions are set to their definitions.
+// 4.Next it creates the execution phase
+// 5.Execution phase - In this phase,the variables are initialsed to given values and 
+//   when the function s are called,it creates a memory phase and execution phase for the function 
+//   just like the main one.
