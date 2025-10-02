29156a8cb5a7026ab1fea65e0850f70ced22330a
shubham-92 <158573766+shubham-92@users.noreply.github.com>
2025-10-02 14:01:08 +0000

    switch and truthy

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,25 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+let month=3
+switch (month) {
+    case 1:
+        console.log("January");
+        
+    case 2:
+        console.log("January");
+        
+    case 3:
+        console.log("March");
+        
+        break;
+
+    default:
+        console.log("defalut")
+        break;
+}
